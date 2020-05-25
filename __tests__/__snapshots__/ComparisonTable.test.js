112f613f1016e2124e57415f6577006d63d6e12b
Jane <wangjing27@outlook.com>
2020-05-26 09:52:54 +1200

    update

--- __tests__/__snapshots__/ComparisonTable.test.js
+++ __tests__/__snapshots__/ComparisonTable.test.js
@@ -5,7 +5,6 @@ describe('WithMenu', () => {
   it('Should 16 value in the table', () => {
     let output = shallow(CompareTItems());
     expect(output).toMatchSnapshot();
-    console.log(output.debug())
     expect(output.find('value').first().text()).toEqual('Name');
     expect(output.find('value')).toHaveLength(16);
     expect(output.find('value').last().text()).toEqual('250');
